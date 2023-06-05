56ed7b03ef459ec8038fe019b334d0d8f9966b60
Cris Kith <cristiankith@gmail.com>
2023-06-05 15:23:56 +0100

    changes for the edit profile

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
- 
+
 function Navbar() {
   return (
     <nav>
       <Link to="/">
         <button>Home</button>
       </Link>
- 
+
       <Link to="/offers">
         <button>Books Available</button>
       </Link>
@@ -22,12 +22,16 @@ function Navbar() {
       <Link to="/login">
         <button>Login</button>
       </Link>
-{/* temporary, until we add conditional rendering */}
+      {/* temporary, until we add conditional rendering */}
       <Link to="/profile">
         <button>Profile</button>
+      </Link>
+
+      <Link to="/editprofile">
+        <button>Edit Profile</button>
       </Link>
     </nav>
   );
 }
- 
+
 export default Navbar;
--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,59 +1,56 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import authService from "../Services/auth.service";
 
-// Creates React Context with shareable State data
 const AuthContext = React.createContext();
 
 function AuthProviderWrapper(props) {
-  // Write State
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
 
-  // Store Token function
   const storeToken = (token) => {
     localStorage.setItem("authToken", token);
   };
 
-  // Authentication Function
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+  };
+
+  const logoutUser = () => {
+    removeToken();
+    authenticateUser();
+  };
+
+  const getToken = () => {
+    console.log(localStorage.getItem("authToken"));
+    return localStorage.getItem("authToken");
+  };
+
   const authenticateUser = () => {
-    // Get the Stored Token from Local Storage
     const storedToken = localStorage.getItem("authToken");
+
     if (storedToken) {
-      authService
-        .verify()
+      axios
+        .get(`${import.meta.env.VITE_APP_SERVER_URL}/api/verify`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
         .then((response) => {
-          //Update state variables
           setIsLoggedIn(true);
           setIsLoading(false);
-          setUser(response.data.user);
+          setUser(response.data);
         })
-        .catch(() => {
-          // catch possibility whenever it finds an invalid token
+        .catch((err) => {
           setIsLoggedIn(false);
           setIsLoading(false);
           setUser(null);
         });
     } else {
-      // If the token was not found on localStorage
       setIsLoggedIn(false);
       setIsLoading(false);
       setUser(null);
     }
-  };
-
-  // Remove Token Function
-  const removeToken = () => {
-    //Upon Logout, Remove Token from the localStorage
-    localStorage.removeItem("authToken");
-  };
-
-  const logOutUser = () => {
-    // removing the JWT token from the localStorage
-    removeToken();
-    // to update state variables
-    authenticateUser();
   };
 
   useEffect(() => {
@@ -68,7 +65,8 @@ function AuthProviderWrapper(props) {
         user,
         storeToken,
         authenticateUser,
-        logOutUser,
+        logoutUser,
+        getToken,
       }}
     >
       {props.children}
@@ -76,4 +74,4 @@ function AuthProviderWrapper(props) {
   );
 }
 
-export { AuthProviderWrapper, AuthContext };
+export { AuthContext, AuthProviderWrapper };
